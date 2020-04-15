1f78bb5e65904fa761b082b1263a38b3e4ce49ed
hidaris <zuocool@gmail.com>
2020-04-15 20:55:15 +0800

    bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 
 setup(
     name="aiowebthing",
-    version="0.1.0",
+    version="0.1.1",
     description="HTTP Web Thing async implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
